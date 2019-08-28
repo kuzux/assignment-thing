da549fed48be11f4133b750f9b7664df3102fb21
Arda Cinar <kuzux92@gmail.com>
2019-08-28 14:43:54 +0300

    No filter query works

--- index.js
+++ index.js
@@ -1,18 +1,36 @@
-const mongo = require("mongodb");
-const MongoClient = mongo.MongoClient;
+const mongoose = require('mongoose');
 const express = require("express");
+const bodyParser = require("body-parser");
 
 const port = 3000;
 const app = express();
+app.use(bodyParser.json());
+
+mongoose.connect(process.env.DB_URL, {useNewUrlParser: true});
+const db = mongoose.connection;
+db.on('error', (err) => { console.log(`connection error: ${err}`); });
+db.once('open', () => { console.log("connected to db"); });
+
+const schema = new mongoose.Schema({
+    createdAt: Date,
+    counts: [Number]
+}, {
+    collection: 'records'
+});
+const Record = mongoose.model('Record', schema);
 
 app.post('/filter', (req, res) => {
-    res.send('hello!')
+    console.log(req.body);
+    const recs = Record.find({
+    });
+    recs.exec((err, docs) => {
+        if(err) {
+            console.log(`query error: ${err}`);
+            res.sendStatus(500);
+        } else {
+            res.send(docs);
+        }
+    });
 });
 
-MongoClient.connect(process.env.DB_URL, { useNewUrlParser: true,useUnifiedTopology: true }, (err, client) => {
-    if (err) throw err;
-    console.log(client.topology.clientInfo);
-    app.listen(port, () => console.log(`Listening on port ${port}!`))
-
-    client.close();
-});
+app.listen(port, () => console.log(`Listening on port ${port}!`))
